ec7881909867629cd928c310c98a083e83844e0a
Kadyrov Alexandr <a.kadyrov@qiwi.com>
2018-05-06 02:14:56 +0500

    add framework

---
+++ SWRevealViewController/SWRevealViewController/SWRevealViewController.h
@@ -0,0 +1,19 @@
+//
+//  SWRevealViewController.h
+//  SWRevealViewController
+//
+//  Created by Alexander on 06.05.2018.
+//  Copyright © 2018 solutions-in.cloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWRevealViewController.
+FOUNDATION_EXPORT double SWRevealViewControllerVersionNumber;
+
+//! Project version string for SWRevealViewController.
+FOUNDATION_EXPORT const unsigned char SWRevealViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWRevealViewController/PublicHeader.h>
+
+
