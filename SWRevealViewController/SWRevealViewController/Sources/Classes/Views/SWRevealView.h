f31bb299efca2e12a3e875327c13a71a53da7163
Kadyrov Alexandr <a.kadyrov@qiwi.com>
2018-05-06 02:25:29 +0500

    add SWRevealView class

---
+++ SWRevealViewController/SWRevealViewController/Sources/Classes/Views/SWRevealView.h
@@ -0,0 +1,13 @@
+//
+//  SWRevealView.h
+//  SWRevealViewController
+//
+//  Created by Alexander on 06.05.2018.
+//  Copyright © 2018 solutions-in.cloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWRevealView : UIView
+
+@end
---
+++ SWRevealViewController/SWRevealViewController/Sources/Classes/Views/SWRevealView.m
@@ -0,0 +1,15 @@
+//
+//  SWRevealView.m
+//  SWRevealViewController
+//
+//  Created by Alexander on 06.05.2018.
+//  Copyright © 2018 solutions-in.cloud. All rights reserved.
+//
+
+#import "SWRevealView.h"
+
+@implementation SWRevealView
+
+
+
+@end
